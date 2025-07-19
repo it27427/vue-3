53b0b39f25f4b54421c19003c584cfee35a92688
it27427 <istiak.tushar.qtec@gmail.com>
2025-07-19 22:00:58 +0600

    alias configured

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,9 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import Test from "@/components/Test.vue";
+</script>
 
 <template>
-  <div></div>
+  <div>
+    <Test />
+  </div>
 </template>
--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,12 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import path from "path";
+import { fileURLToPath } from "url";
+import { dirname } from "path";
 
-// https://vite.dev/config/
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 export default defineConfig({
   plugins: [vue()],
   server: {
@@ -11,18 +15,16 @@ export default defineConfig({
   css: {
     preprocessorOptions: {
       scss: {
-        additionalData: `
-          @import "@/style.scss";
-        `,
+        additionalData: `@import "@/style.scss";`,
       },
     },
   },
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"),
-      "@assets": path.resolve(__dirname, "./src/assets"),
-      "@views": path.resolve(__dirname, "./src/views"),
-      "@components": path.resolve(__dirname, "./src/components"),
+      "@": path.resolve(__dirname, "src"),
+      "@assets": path.resolve(__dirname, "src/assets"),
+      "@views": path.resolve(__dirname, "src/views"),
+      "@components": path.resolve(__dirname, "src/components"),
     },
   },
 });
