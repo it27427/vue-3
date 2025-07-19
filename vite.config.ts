4a08571e6a6d0317d03f90ab40ea322a28c10cd5
it27427 <istiak.tushar.qtec@gmail.com>
2025-07-19 21:19:11 +0600

    sass installed and added then alias configured

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,28 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import path from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
   server: {
     port: 3000,
-  }
-})
+  },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: `
+          @import "@/style.scss";
+        `,
+      },
+    },
+  },
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+      "@assets": path.resolve(__dirname, "./src/assets"),
+      "@views": path.resolve(__dirname, "./src/views"),
+      "@components": path.resolve(__dirname, "./src/components"),
+    },
+  },
+});
