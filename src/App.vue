7a6b6e2d749f79abdae168aa163a02aaf4497fd1
it27427 <istiak.tushar.qtec@gmail.com>
2025-07-21 00:26:02 +0600

    Creating Main Menu

--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,21 @@ import Test from "@/components/Test.vue";
 
 <template>
   <div>
-    <Test hello="Hello" />
+    <ul>
+      <li>
+        <router-link to="/"> Home </router-link>
+      </li>
+      <li>
+        <router-link to="/about"> About </router-link>
+      </li>
+      <li>
+        <router-link to="/contact"> Contact </router-link>
+      </li>
+      <li>
+        <router-link to="/store"> Store </router-link>
+      </li>
+    </ul>
+
+    <router-view></router-view>
   </div>
 </template>
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -8,21 +8,23 @@ import Store from "@/views/Store.vue";
 
 import NotFound from "@/views/NotFound.vue";
 
+const routes = [
+  { path: "/", name: "Home", component: Home },
+
+  { path: "/about", name: "About", component: About },
+
+  { path: "/contact", name: "Contact", component: Contact },
+
+  { path: "/store", name: "Store", component: Store },
+
+  // Catch-all route for 404 Not Found
+  { path: "/*", name: "NotFound", component: NotFound },
+];
+
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHistory(), // import.meta.env.BASE_URL
 
-  routes: [
-    { path: "/", name: "Home", component: Home },
-
-    { path: "/about", name: "About", component: About },
-
-    { path: "/contact", name: "Contact", component: Contact },
-
-    { path: "/store", name: "Store", component: Store },
-
-    // Catch-all route for 404 Not Found
-    { path: "*", name: "NotFound", component: NotFound },
-  ],
+  routes,
 });
 
 export default router;
