e0a3e383c9ffbd73ec95a6b7fb02faf98789dc58
it27427 <istiak.tushar.qtec@gmail.com>
2025-07-21 15:25:55 +0600

    MainMenu navItems Dynamic

--- src/components/global/MainMenu.vue
+++ src/components/global/MainMenu.vue
@@ -1,3 +1,13 @@
+<script setup lang="ts">
+const navItems = [
+  { name: "Home", to: "/" },
+  { name: "About", to: "/about" },
+  { name: "Blog", to: "/blog" },
+  { name: "Store", to: "/store" },
+  { name: "Contact", to: "/contact" },
+];
+</script>
+
 <template>
   <nav class="flex items-center justify-between p-4 bg-gray-800">
     <figure class="flex items-center justify-center">
@@ -7,44 +17,12 @@
     </figure>
 
     <ul class="flex items-center space-x-4">
-      <li>
+      <li v-for="item in navItems" :key="item.name">
         <router-link
-          to="/"
+          :to="item.to"
           class="text-gray-50 hover:text-green-200 uppercase transition duration-500 ease-in-out"
         >
-          Home
-        </router-link>
-      </li>
-      <li>
-        <router-link
-          to="/about"
-          class="text-gray-50 hover:text-green-200 uppercase transition duration-500 ease-in-out"
-        >
-          About
-        </router-link>
-      </li>
-      <li>
-        <router-link
-          to="/blog"
-          class="text-gray-50 hover:text-green-200 uppercase transition duration-500 ease-in-out"
-        >
-          Blog
-        </router-link>
-      </li>
-      <li>
-        <router-link
-          to="/store"
-          class="text-gray-50 hover:text-green-200 uppercase transition duration-500 ease-in-out"
-        >
-          Store
-        </router-link>
-      </li>
-      <li>
-        <router-link
-          to="/contact"
-          class="text-gray-50 hover:text-green-200 uppercase transition duration-500 ease-in-out"
-        >
-          Contact
+          {{ item.name }}
         </router-link>
       </li>
     </ul>
