5d0c48ff4b62f8930bbb69f293e56fc8166a7c0e
it27427 <istiak.tushar.qtec@gmail.com>
2025-07-23 22:29:56 +0600

    ContactForm Created and Added in Contact Page

---
+++ src/components/ContactForm.vue
@@ -0,0 +1,41 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <form class="flex flex-col gap-5">
+    <div class="flex flex-col gap-3">
+      <label for="name">Enter Your Name</label>
+      <input type="text" id="name" name="name" class="border border-gray-300 rounded p-2"
+        placeholder="Enter Full Name" />
+    </div>
+
+    <div class="flex flex-col gap-3">
+      <label for="email">Enter Your E-mail</label>
+      <input type="email" id="email" name="email" class="border border-gray-300 rounded p-2"
+        placeholder="Enter Email Address" />
+    </div>
+
+    <div class="flex flex-col gap-3">
+      <label for="phone">Enter Your Phone Number</label>
+      <input type="tel" id="phone" name="phone" class="border border-gray-300 rounded p-2"
+        placeholder="Enter Phone Number" />
+    </div>
+
+    <div class="flex flex-col gap-3">
+      <label for="subject">Enter Your Subject</label>
+      <input type="text" id="subject" name="subject" class="border border-gray-300 rounded p-2"
+        placeholder="Enter Subject" />
+    </div>
+
+    <div class="flex flex-col gap-3">
+      <label for="message">Enter Your Message</label>
+      <textarea id="message" name="message" class="border border-gray-300 rounded p-2" placeholder="Enter Your Message"
+        rows="5"></textarea>
+    </div>
+
+    <button type="submit" class="btn-lg border-t-cyan-400 transition-all hover:bg-cyan-400">
+      Send Message
+    </button>
+  </form>
+</template>
--- src/views/Contact.vue
+++ src/views/Contact.vue
@@ -1,7 +1,11 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import ContactForm from "@/components/ContactForm.vue";
+</script>
 
 <template>
   <div>
     <h1 class="text-5xl font-bold text-black">Contact Page</h1>
+
+    <ContactForm />
   </div>
 </template>
