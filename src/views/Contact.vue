2d68f16df7b63d80fd8d9466f098eb630888f1b2
it27427 <istiak.tushar.qtec@gmail.com>
2025-07-26 18:32:37 +0600

    Contact.vue Title and header title and description dynamically added

--- src/views/Contact.vue
+++ src/views/Contact.vue
@@ -1,13 +1,17 @@
 <script setup lang="ts">
 import ContactForm from "@/components/ContactForm.vue";
+import { ref } from "vue";
+
+const title = ref("Get In Touch");
+const description = ref("We would love to hear from you!");
 </script>
 
 <template>
   <div class="container max-w-7xl px-4 pt-8 pb-4">
     <div class="mb-8">
       <div class="flex flex-col items-center justify-center gap-3 pb-5">
-        <h1 class="text-5xl font-bold text-black">Get In Touch</h1>
-        <p class="text-gray-600">We would love to hear from you!</p>
+        <h1 class="text-5xl font-bold text-black">{{ title }}</h1>
+        <p class="text-gray-600">{{ description }}</p>
       </div>
 
       <ContactForm />
