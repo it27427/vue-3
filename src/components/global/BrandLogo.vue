180876026d8741b3b7553f717f02452fbccad8af
it27427 <istiak.tushar.qtec@gmail.com>
2025-07-26 18:44:28 +0600

    BrandLogo Component Created Then Added In MainMenu.vue

---
+++ src/components/global/BrandLogo.vue
@@ -0,0 +1,7 @@
+<template>
+  <figure class="flex items-center justify-center">
+    <router-link to="/">
+      <img src="@/assets/vue.svg" alt="Brand Logo" class="h-8 w-8" />
+    </router-link>
+  </figure>
+</template>
--- src/components/global/MainMenu.vue
+++ src/components/global/MainMenu.vue
@@ -1,14 +1,11 @@
 <script setup lang="ts">
+import BrandLogo from "@/components/global/BrandLogo.vue";
 import MainList from "@/components/global/MainList.vue";
 </script>
 
 <template>
   <nav class="flex items-center justify-between p-4 bg-gray-800 shadow">
-    <figure class="flex items-center justify-center">
-      <router-link to="/">
-        <img src="@/assets/vue.svg" alt="Brand Logo" class="h-8 w-8" />
-      </router-link>
-    </figure>
+    <BrandLogo />
 
     <MainList />
   </nav>
