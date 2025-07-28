7e49129ac2c057bde005236d61ca857aa6557baa
it27427 <istiak.tushar.qtec@gmail.com>
2025-07-29 02:04:07 +0600

    ChangePasswordForm, ChangePassword Page Markup Completed

--- src/components/auth/ChangePasswordForm.vue
+++ src/components/auth/ChangePasswordForm.vue
@@ -3,7 +3,21 @@
 </script>
 
 <template>
-  <div>
+  <form class="max-w-md mx-auto mt-10 p-6 bg-white shadow-md rounded-lg flex flex-col gap-4">
+    <div>
+      <label for="password" class="block text-sm font-medium text-gray-700">
+        New Password <span class="text-red-500">*</span>
+      </label>
+      <input type="password" id="newPassword" name="newPassword" required
+        class="mt-1 block w-full border-gray-300 rounded-md shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm">
+    </div>
 
-  </div>
+    <div>
+      <label for="confirmNewPassword" class="block text-sm font-medium text-gray-700">
+        Confirm New Password <span class="text-red-500">*</span>
+      </label>
+      <input type="password" id="confirmNewPassword" name="confirmNewPassword" required
+        class="mt-1 block w-full border-gray-300 rounded-md shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm">
+    </div>
+  </form>
 </template>
--- src/views/auth/ChangePassword.vue
+++ src/views/auth/ChangePassword.vue
@@ -1,9 +1,15 @@
 <script setup lang="ts">
+import { ref } from 'vue';
 
+const title = ref('Create new password');
+const description = ref('Your new password must be unique from those previously used.');
 </script>
 
 <template>
   <div>
+    <h1 class="text-5xl font-bold text-black">{{ title }}</h1>
+    <p class="text-lg text-gray-600">{{ description }}</p>
 
+    <!-- Add change password form here -->
   </div>
 </template>
